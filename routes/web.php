fe93c549409ec67646fbf052196d2e72d907013c
Manish4090 <nickykumar.1010@gmail.com>
2021-10-04 11:05:03 +0530

    new types releations

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,36 @@ Route::get('/add-machanic', [MachanicController::class, 'add_machanic']);
 Route::get('/add-car/{id}', [CarController::class, 'add_car']);
 Route::get('/add-owner/{id}', [OwnerController::class, 'add_owner']);
 Route::get('/show-owner/{id}', [MachanicController::class, 'show_owner']);
+
+
+#### Relation ######
+
+Route::get('one-to-one',[Relations::class,'oneToOne'] );
+Route::get('inverse-one-to-one',[Relations::class,'inverseOneToOne'] );
+
+
+Route::get('one-to-many',[Relations::class,'oneToMany'] );
+
+Route::get('many-to-many-blog',[Relations::class,'manyToManyBlog'] );
+
+Route::get('many-to-many-tag',[Relations::class,'manyToManyTag'] );
+
+Route::get('one-to-many-through',[Relations::class,'oneToManyThrough'] );
+
+Route::get('many-to-many-through',[Relations::class,'manyToManyThrough'] );
+
+
+Route::get('one-to-one-ploy-blog',[Relations::class,'oneToOnePloyBlog'] );
+
+Route::get('one-to-one-ploy-client',[Relations::class,'oneToOnePloyClient'] );
+
+Route::get('one-to-one-ploy-image',[Relations::class,'oneToOnePloyImage'] );
+
+Route::get('one-to-one-ploy-blogs',[Relations::class,'oneToOnePloyBlogs'] );
+
+Route::get('one-to-one-ploy-clients',[Relations::class,'oneToOnePloyClients'] );
+
+
+Route::get('many-to-many-ploy-blog',[Relations::class,'manyToManyPloyBlog'] ); 
+Route::get('many-to-many-ploy-video',[Relations::class,'manyToManyPloyVideo'] );
+Route::get('many-to-many-ploy-tag',[Relations::class,'manyToManyPloyTag'] );
